e58ee64dc694ebfe8910ff7ebb49fef5ef1d80bd
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2025-04-16 11:05:12 -0300

    feat: add end-to-end tests for CreateUserController and configure Vitest for coverage

--- src/modules/identity/http/controllers/__tests__/create-user.controller.e2e-spec.ts
+++ src/modules/identity/http/controllers/__tests__/create-user.controller.e2e-spec.ts
@@ -1,3 +1,83 @@
-test('1 plus 1', () => {
-  expect(1 + 1).toBe(2);
+import { BadRequestException, INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+import { AppModule } from '@/app.module';
+import { PrismaService } from '@/shared/database/prisma.service';
+import { CreateUserUseCase } from '@/modules/identity/core/use-cases/create-user.use-case';
+import { left } from '@/modules/_shared/utils/either';
+
+describe('CreateUserController (E2E)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    prisma = moduleRef.get(PrismaService);
+
+    await app.init();
+  });
+
+  test('[POST] /users', async () => {
+    const response = await request(app.getHttpServer()).post('/users').send({
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: 'Test@123',
+    });
+
+    expect(response.statusCode).toBe(201);
+  });
+
+  test('[POST] /users - ConflictException', async () => {
+    prisma.user.create({
+      data: {
+        id: 'id',
+        name: 'John Doe',
+        email: 'john@example.com',
+        password: 'Test@123',
+      },
+    });
+
+    const response = await request(app.getHttpServer()).post('/users').send({
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: 'Test@123',
+    });
+
+    expect(response.statusCode).toBe(409);
+  });
+});
+
+describe('CreateUserController (E2E) ERROR', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const mockUseCase = {
+      execute: vi.fn().mockResolvedValue(left(new BadRequestException())),
+    };
+
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider(CreateUserUseCase)
+      .useValue(mockUseCase)
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  test('[POST] /users - BadRequest', async () => {
+    const response = await request(app.getHttpServer()).post('/users').send({
+      name: 'John Doe',
+      email: 'unexpected@error.com',
+      password: 'Test@123',
+    });
+
+    expect(response.statusCode).toBe(400);
+  });
 });
